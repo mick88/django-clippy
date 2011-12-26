d60c8ea953a3db90c78b81b624d0442982e2ae9d
Ludovic Gasc <gmludo@gmail.com>
2011-12-26 15:45:10 +0100

    Remove double slashes

--- clippy/templatetags/clippy.py
+++ clippy/templatetags/clippy.py
@@ -22,13 +22,13 @@ def clippy(htmlElementId, bgcolor='#ffffff'):
                 width="110"
                 height="14"
                 id="clippy_%(htmlElementId)s" >
-        <param name="movie" value="%(media_url)s/clippy.swf"/>
+        <param name="movie" value="%(media_url)sclippy.swf"/>
         <param name="allowScriptAccess" value="always" />
         <param name="quality" value="high" />
         <param name="scale" value="noscale" />
         <param NAME="FlashVars" value="id=%(htmlElementId)s">
         <param name="bgcolor" value="%(bgcolor)s">
-        <embed src="%(media_url)s/clippy.swf?x=23"
+        <embed src="%(media_url)sclippy.swf?x=23"
                width="110"
                height="14"
                name="clippy"
